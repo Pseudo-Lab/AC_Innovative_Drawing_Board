7dd51114ef05ec2216bd64a26c0a0a67e67cbf3f
윤성민 <sungmin1551@naver.com>
2022-03-17 18:43:20 +0900

    0.0.02v
    path.py 경로 모음 파일 추가

--- Git Branch Test.py
+++ Git Branch Test.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 # Git branch develop test
-=======
-Git branch develop test
->>>>>>> d775050c7ba2499b51453be0fa60b7bff5346ef1
+# =======
+# Git branch develop test
+# >>>>>>> d775050c7ba2499b51453be0fa60b7bff5346ef1
