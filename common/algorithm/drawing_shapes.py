77c2104fd8f8b0b66313f63a60a3678675649231
xellnaga <woongbae@gmail.com>
2022-04-08 23:50:35 +0900

    add drawing shapes - minRect, boundingRect, circle

--- common/algorithm/drawing_shapes.py
+++ common/algorithm/drawing_shapes.py
@@ -1,14 +1,36 @@
 import numpy as np
 import math
+import cv2
 
-def draw_ellipse(ecenter, esize, erotation):
-    cx, cy = ecenter[0], ecenter[1]
-    rx, ry = esize[0], esize[1]
+def draw_ellipse(points):
+    rrt = cv2.fitEllipse(points)
+    cx, cy = rrt[0][0], rrt[0][1]
+    rx, ry = rrt[1][0], rrt[1][1]
     listofpoints = []
-    rad = erotation * 3.14 / 180
+    rad = rrt[2] * 3.14 / 180
     for i in np.arange(0, 2 * math.pi, 0.1):
         x = cx - (ry * 0.5 * math.sin(i)) * math.sin(rad) + (rx * 0.5 * math.cos(i)) * math.cos(rad)
         y = cy + (rx * 0.5 * math.cos(i)) * math.sin(rad) + (ry * 0.5 * math.sin(i)) * math.cos(rad)
         listofpoints.append([x, y])
 
     return listofpoints
+
+def draw_minRect(points):
+    rect = cv2.minAreaRect(points)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    return box
+
+def draw_boundingRect(points):
+    brect = cv2.boundingRect(points)
+
+    return brect
+
+def draw_circle(points):
+    (x, y), radius = cv2.minEnclosingCircle(points)
+    circle_points = []
+    for i in np.arange(0, 2 * math.pi, 0.1):
+        #ang = 2 * math.pi * i
+        circle_points.append([math.sin(i) * radius + x, math.cos(i) * radius + y])
+
+    return circle_points
--- work/view/first.py
+++ work/view/first.py
@@ -4,7 +4,7 @@ from config import setting
 import numpy as np
 import cv2
 #import math
-from common.algorithm.drawing_shapes import draw_ellipse
+from common.algorithm.drawing_shapes import *
 
 
 class First(QtWidgets.QGraphicsView):
@@ -84,7 +84,7 @@ class First(QtWidgets.QGraphicsView):
             self.q_graphic.setPixmap(canvas)
 
             ret = np.array(self.pointlist)
-            rrt = cv2.fitEllipse(ret)
+
 
             # 캔버스 가져오기
             canvas = self.q_graphic.pixmap()
@@ -94,7 +94,7 @@ class First(QtWidgets.QGraphicsView):
             painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
 
             # 타원 그리기
-            listofpoints = draw_ellipse(rrt[0], rrt[1], rrt[2])
+            listofpoints = draw_ellipse(ret)
             firstPoint =  QtCore.QPoint()
             for count, item in enumerate(listofpoints):#, start=1):
                 if count == 0:
@@ -104,6 +104,138 @@ class First(QtWidgets.QGraphicsView):
                     painter.drawLine(self.lastPoint, QtCore.QPoint(item[0], item[1]))
                     self.lastPoint = QtCore.QPoint(item[0], item[1])
             painter.drawLine(self.lastPoint, firstPoint)
+
+            painter.end()
+
+            self.q_graphic.setPixmap(canvas)
+            self.pointlist.clear()
+
+        if self.draw_state is 'circle':
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 지우개 생성
+            rubber = QtGui.QPen()
+            a: int = 10
+            rubber.setWidth(a)
+            rubber.setColor(QtGui.Qt.white)
+
+            # 지우개 장착
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(rubber)
+
+            for rpoint in self.pointlist:
+                painter.drawPoint(QtCore.QPoint(rpoint[0][0],rpoint[0][1]))
+            painter.end()
+            self.q_graphic.setPixmap(canvas)
+
+            ret = np.array(self.pointlist)
+
+
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 페인트 생성
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
+
+            # 원 그리기
+            listofpoints = draw_circle(ret)
+            firstPoint =  QtCore.QPoint()
+            for count, item in enumerate(listofpoints):#, start=1):
+                if count == 0:
+                    self.lastPoint = QtCore.QPoint(item[0], item[1])
+                    firstPoint = QtCore.QPoint(item[0], item[1])
+                else:
+                    painter.drawLine(self.lastPoint, QtCore.QPoint(item[0], item[1]))
+                    self.lastPoint = QtCore.QPoint(item[0], item[1])
+            painter.drawLine(self.lastPoint, firstPoint)
+
+            painter.end()
+
+            self.q_graphic.setPixmap(canvas)
+            self.pointlist.clear()
+
+        if self.draw_state is 'minRect':
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 지우개 생성
+            rubber = QtGui.QPen()
+            a: int = 10
+            rubber.setWidth(a)
+            rubber.setColor(QtGui.Qt.white)
+
+            # 지우개 장착
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(rubber)
+
+            for rpoint in self.pointlist:
+                painter.drawPoint(QtCore.QPoint(rpoint[0][0],rpoint[0][1]))
+            painter.end()
+            self.q_graphic.setPixmap(canvas)
+
+            ret = np.array(self.pointlist)
+
+
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 페인트 생성
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
+
+            # minRect 그리기
+            boxx = draw_minRect(ret)
+
+            #draw minRect
+            painter.drawLine( QtCore.QPoint(boxx[0][0], boxx[0][1]),  QtCore.QPoint(boxx[1][0], boxx[1][1]))
+            painter.drawLine( QtCore.QPoint(boxx[1][0], boxx[1][1]),  QtCore.QPoint(boxx[2][0], boxx[2][1]))
+            painter.drawLine( QtCore.QPoint(boxx[2][0], boxx[2][1]),  QtCore.QPoint(boxx[3][0], boxx[3][1]))
+            painter.drawLine( QtCore.QPoint(boxx[3][0], boxx[3][1]),  QtCore.QPoint(boxx[0][0], boxx[0][1]))
+            painter.end()
+
+            self.q_graphic.setPixmap(canvas)
+            self.pointlist.clear()
+
+        if self.draw_state is 'boundingRect':
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 지우개 생성
+            rubber = QtGui.QPen()
+            a: int = 10
+            rubber.setWidth(a)
+            rubber.setColor(QtGui.Qt.white)
+
+            # 지우개 장착
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(rubber)
+
+            for rpoint in self.pointlist:
+                painter.drawPoint(QtCore.QPoint(rpoint[0][0],rpoint[0][1]))
+            painter.end()
+            self.q_graphic.setPixmap(canvas)
+
+            ret = np.array(self.pointlist)
+
+
+            # 캔버스 가져오기
+            canvas = self.q_graphic.pixmap()
+
+            # 페인트 생성
+            painter = QtGui.QPainter(canvas)
+            painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
+
+            # boundingRect 그리기
+            brect = draw_boundingRect(ret)
+
+            #draw boundingRect
+            painter.drawLine(QtCore.QPoint(brect[0], brect[1]), QtCore.QPoint(brect[0] + brect[2], brect[1]))
+            painter.drawLine(QtCore.QPoint(brect[0] + brect[2], brect[1]), QtCore.QPoint(brect[0] + brect[2], brect[1] + brect[3]))
+            painter.drawLine(QtCore.QPoint(brect[0] + brect[2], brect[1] + brect[3]),QtCore.QPoint(brect[0], brect[1] + brect[3]))
+            painter.drawLine(QtCore.QPoint(brect[0], brect[1] + brect[3]), QtCore.QPoint(brect[0], brect[1]))
+
             painter.end()
 
             self.q_graphic.setPixmap(canvas)
