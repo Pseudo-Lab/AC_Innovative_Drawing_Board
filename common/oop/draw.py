8660dc029a55e0ea74bbbeca9a1e7e625e7e1516
DESKTOP-24R2K26\nccbme1221 <sungmin1551@naver.com>
2022-04-12 08:33:34 +0900

    draw.py fix
    canvas -> self.canvas

--- common/oop/draw.py
+++ common/oop/draw.py
@@ -63,7 +63,7 @@ class Draw:
         canvas = self.q_graphic.pixmap()
 
         # 페인트 생성
-        painter = QtGui.QPainter(canvas)
+        painter = QtGui.QPainter(self.canvas)
         painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
 
         # 원 그리기
@@ -80,18 +80,15 @@ class Draw:
 
         painter.end()
 
-        self.q_graphic.setPixmap(canvas)
+        self.q_graphic.setPixmap(self.canvas)
         self.pointlist.clear()
 
     def rotated_rect(self):
         self.removePrevPoints()
         ret = np.array(self.pointlist)
 
-        # 캔버스 가져오기
-        canvas = self.q_graphic.pixmap()
-
         # 페인트 생성
-        painter = QtGui.QPainter(canvas)
+        painter = QtGui.QPainter(self.canvas)
         painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
 
         # minRect 그리기
@@ -102,20 +99,18 @@ class Draw:
         painter.drawLine(QtCore.QPoint(boxx[1][0], boxx[1][1]), QtCore.QPoint(boxx[2][0], boxx[2][1]))
         painter.drawLine(QtCore.QPoint(boxx[2][0], boxx[2][1]), QtCore.QPoint(boxx[3][0], boxx[3][1]))
         painter.drawLine(QtCore.QPoint(boxx[3][0], boxx[3][1]), QtCore.QPoint(boxx[0][0], boxx[0][1]))
+
         painter.end()
 
-        self.q_graphic.setPixmap(canvas)
+        self.q_graphic.setPixmap(self.canvas)
         self.pointlist.clear()
 
     def rect(self):
         self.removePrevPoints()
         ret = np.array(self.pointlist)
 
-        # 캔버스 가져오기
-        canvas = self.q_graphic.pixmap()
-
         # 페인트 생성
-        painter = QtGui.QPainter(canvas)
+        painter = QtGui.QPainter(self.canvas)
         painter.setPen(QtGui.QPen(QtCore.Qt.red, 1, QtCore.Qt.SolidLine))
 
         # boundingRect 그리기
@@ -131,7 +126,7 @@ class Draw:
 
         painter.end()
 
-        self.q_graphic.setPixmap(canvas)
+        self.q_graphic.setPixmap(self.canvas)
         self.pointlist.clear()
 
     def pen(self, e):
